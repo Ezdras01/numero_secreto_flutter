fbeaf65ce8c936c7822aebfcd29a03fc58aacc9b
Ezra Cortez <lehi170993@gmail.com>
2025-05-15 14:13:31 -0600

    feat: método auxiliar
    este método nos  permitirá mandar un mensaje al usuario para saber si debe intentar con un numero mayor o menor o si logro acertar.

--- numero_secreto_flutter/lib/models/intento_resultado.dart
+++ numero_secreto_flutter/lib/models/intento_resultado.dart
@@ -11,4 +11,14 @@ class IntentoResultado {
     required this.tipo,
   });
   
+  String comoTexto() {
+    switch (tipo) {
+      case TipoResultado.mayor:
+        return 'Mayor que el número secreto';
+      case TipoResultado.menor:
+        return 'Menor que el número secreto';
+      case TipoResultado.correcto:
+        return '¡Correcto!';
+    }
+  }
   }//el que cierra
