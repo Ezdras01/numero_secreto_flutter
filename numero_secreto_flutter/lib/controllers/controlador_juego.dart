1de47b36989c34298c1c4d23260880fafd549e74
Ezra Cortez <lehi170993@gmail.com>
2025-05-15 14:38:18 -0600

    doc: creación de listas
    listas para las columnas

--- numero_secreto_flutter/lib/controllers/controlador_juego.dart
+++ numero_secreto_flutter/lib/controllers/controlador_juego.dart
@@ -13,9 +13,10 @@ class ControladorJuego {
   late int _intentosRestantes = 0;
   bool _juegoTerminado = false;
 
+// creacion de listas privadas para las columnas "mayor que" y "menor que"
   final List<int> _numerosMayores = [];
   final List<int> _numerosMenores = [];
-
+//creacion de lista privada para el historial de intentos 
   final List<Map<String, dynamic>> _historial = [];
 
 
