d8f0d54d9f8f481f91e727152cd6717fbd6bda22
Ezra Cortez <lehi170993@gmail.com>
2025-05-15 14:10:49 -0600

    feat: clase IntentoResultado
    con esta clase representamos el intento hecho por un usuario(numero que ingreso, tipo de resultado del intento) y el constructor para inicializar un intento con su numero y resultado.

--- numero_secreto_flutter/lib/models/intento_resultado.dart
+++ numero_secreto_flutter/lib/models/intento_resultado.dart
@@ -1,2 +1,14 @@
 
 enum TipoResultado { mayor, menor, correcto}
+
+class IntentoResultado {
+  final int numeroIngresado;
+
+  final TipoResultado tipo;
+
+  IntentoResultado({
+    required this.numeroIngresado,
+    required this.tipo,
+  });
+  
+  }//el que cierra
