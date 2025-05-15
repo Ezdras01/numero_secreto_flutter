5fd3e5eed52de4d2f0ef93547f2980a999e25579
Ezra Cortez <lehi170993@gmail.com>
2025-05-15 13:59:12 -0600

    feat:  clase dificultad y constructor
    es la que representa una dificultad con sus propiedades asociadas

--- numero_secreto_flutter/lib/models/dificultad.dart
+++ numero_secreto_flutter/lib/models/dificultad.dart
@@ -1,2 +1,17 @@
 
 enum NivelDificultad { facil, medio, avanzado, extremo }
+
+class Dificultad {
+  final NivelDificultad nivel;
+  final int minimo;
+  final int maximo;
+  final int intentosMaximos;
+
+  Dificultad({
+    required this.nivel,
+    required this.minimo,
+    required this.maximo,
+    required this.intentosMaximos,
+  });
+  
+}// el que cierra 
