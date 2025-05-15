14cc0dd87f10ee6f92c0ecaeae37fff155fd3bad
Ezra Cortez <lehi170993@gmail.com>
2025-05-15 14:29:08 -0600

    feat: clase ControladorJuego
    agregamos unas variables privadas y agregamos un generador de numeros aleatorios.

--- numero_secreto_flutter/lib/controllers/controlador_juego.dart
+++ numero_secreto_flutter/lib/controllers/controlador_juego.dart
@@ -4,3 +4,16 @@ import 'dart:math';
 import 'package:numero_secreto_flutter/models/intento_resultado.dart';
 import 'package:numero_secreto_flutter/models/dificultad.dart';
 
+class ControladorJuego {
+  //generador de numeros aleatorios
+  final Random _aleatorio = Random();
+
+  late Dificultad _dificultadActual;
+  late int _numeroSecreto;
+  late int _intentosRestantes = 0;
+  bool _juegoTerminado = false;
+
+
+
+
+}
