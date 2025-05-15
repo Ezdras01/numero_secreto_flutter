ed32f57f2c72c150bb7b5c048f1b00c17380b5e4
Ezra Cortez <lehi170993@gmail.com>
2025-05-15 14:22:56 -0600

    feat: importamos librerias y otros archivos
    importamos los archivos de "models" para usarlos en la lógica del juego

---
+++ numero_secreto_flutter/lib/controllers/controlador_juego.dart
@@ -0,0 +1,6 @@
+// aqui se encuentra TODA la logica del juego
+
+import 'dart:math';
+import 'package:numero_secreto_flutter/models/intento_resultado.dart';
+import 'package:numero_secreto_flutter/models/dificultad.dart';
+
